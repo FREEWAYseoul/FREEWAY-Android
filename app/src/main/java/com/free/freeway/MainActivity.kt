66c9bab95530012e02a358ee3b33d85894069f82
jcw1031 <jcw001031@gmail.com>
2023-09-14 22:59:53 +0900

    Feat: 위치 권한 + 마이크 권한

--- app/src/main/java/com/free/freeway/MainActivity.kt
+++ app/src/main/java/com/free/freeway/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.free.freeway
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -7,27 +8,28 @@ import android.net.Uri
 import android.os.Bundle
 import android.webkit.*
 import androidx.activity.ComponentActivity
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 
 class MainActivity : ComponentActivity() {
 
-    private var requestPermissionLauncher: ActivityResultLauncher<String>? = null
+    //    private var requestPermissionLauncher: ActivityResultLauncher<String>? = null
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private val MICROPHONE_PERMISSION_REQUEST_CODE = 2
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        requestPermissionLauncher = registerForActivityResult(
-            RequestPermission()
-        ) { isGranted: Boolean ->
-            if (!isGranted) {
-                println("마이크 권한을 허용하지 않으면 음성 검색 기능을 사용하실 수 없습니다!")
-            }
-        }
+        /* requestPermissionLauncher = registerForActivityResult(
+             RequestPermission()
+         ) { isGranted: Boolean ->
+             if (!isGranted) {
+                 println("마이크 권한을 허용하지 않으면 음성 검색 기능을 사용하실 수 없습니다!")
+             }
+         }*/
 
         val webView = findViewById<WebView>(R.id.web_view)
 
@@ -37,23 +39,67 @@ class MainActivity : ComponentActivity() {
         webSettings.domStorageEnabled = true
 
         webView.webViewClient = InternalWebViewClient()
-        webView.loadUrl("http://freewaykr.s3-website.ap-northeast-2.amazonaws.com/")
+        webView.loadUrl("https://freeway-web.vercel.app/")
         webView.webChromeClient = object : WebChromeClient() {
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String,
+                callback: GeolocationPermissions.Callback
+            ) {
+                val permission = Manifest.permission.ACCESS_FINE_LOCATION
+                if (ContextCompat.checkSelfPermission(
+                        this@MainActivity,
+                        permission
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(permission),
+                        LOCATION_PERMISSION_REQUEST_CODE
+                    )
+                    print("위치 권한 허용")
+                } else {
+                    println("이미 위치 권한 허용됨")
+                    callback.invoke(origin, true, false)
+                }
+            }
+
             override fun onPermissionRequest(request: PermissionRequest) {
-                request.grant(request.resources)
+                if (request.resources.contains("android.webkit.resource.AUDIO_CAPTURE")) {
+                    print("마이크 권한 요청")
+                    val permission = Manifest.permission.RECORD_AUDIO
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            permission
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(permission),
+                            MICROPHONE_PERMISSION_REQUEST_CODE
+                        )
+                        print("마이크 권한 허용")
+                    } else {
+                        print("이미 마이크 권한 허용")
+                        request.grant(request.resources)
+                    }
+                }
             }
         }
 
-        requestAudioPermission()
+        print("크롬 설정 끝")
+
+//        requestAudioPermission()
     }
 
-    private fun requestAudioPermission() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
+    /*private fun requestAudioPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.RECORD_AUDIO
+            )
             != PackageManager.PERMISSION_GRANTED
         ) {
-            requestPermissionLauncher!!.launch(android.Manifest.permission.RECORD_AUDIO)
+            requestPermissionLauncher!!.launch(Manifest.permission.RECORD_AUDIO)
         }
-    }
+    }*/
 
     private inner class InternalWebViewClient : WebViewClient() {
         override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
